85eabc332b88eb6f9eac665b0b907f621e69dddb
Clément <duvivier.clem@gmail.com>
2022-05-27 11:38:53 +0200

    :seedling: add files in pages folder

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,6 @@ export default function Home() {
     // </div>
     <div>
       <h1>Titre</h1>
-      <h2>Titre</h2>
-      <h3>Titre</h3>
-      <h4>Titre</h4>
-      <h5>Titre</h5>
-      <p>texte</p>
       <Image src={memojiPic} alt="my avatar making peace pose"/>
     </div>
   )
