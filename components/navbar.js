422823729d90a74a7a2d3e6173ee0bf28e6404e0
Clément <duvivier.clem@gmail.com>
2022-05-31 23:38:43 +0200

    :lipstick: navbar related changes

--- components/navbar.js
+++ components/navbar.js
@@ -4,26 +4,28 @@ import Link from "next/link"
 import MemojiNav from "../public/memojis/memoji_1.svg"
 
 const Navbar =() => (
-    <nav className="nav">
-        <Link href={"/"}>
-            <a>
-                <Image 
-                    className="homeButton"
-                    src={MemojiNav} 
-                    alt="my avatar for nav" 
-                    width = {40} 
-                    height = {50}
-                />
-            </a>
-        </Link>
-        <ul>
-            { Links.map(link => {
-                return(
-                    <li key={link.id}><Link href={link.path}>{link.label}</Link></li>
+    <header>
+        <nav className="nav">
+            <Link href={"/"}>
+                <a>
+                    <Image 
+                        className="homeButton"
+                        src={MemojiNav} 
+                        alt="my avatar for nav" 
+                        width = {40} 
+                        height = {50}
+                    />
+                </a>
+            </Link>
+            <ul>
+                { Links.map(link => {
+                    return(
+                        <li key={link.id}><Link href={link.path}>{link.label}</Link></li>
+                    )}
                 )}
-            )}
-        </ul>
-    </nav>
+            </ul>
+        </nav>
+    </header>
 )
 
 export default Navbar
